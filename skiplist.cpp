f88fb2352abea067bf0dae4307fa6f0889aa0ee6
jqdai <jqdai>
2024-04-02 11:39:33 +0000

    basic class definition of this toy skiplist

--- skiplist.cpp
+++ skiplist.cpp
@@ -2,6 +2,8 @@
 #include<vector>
 
 using std::vector;
+using std::ofstream;
+using std::ifstream;
 
 
 template<typename K, typename V>
@@ -10,7 +12,32 @@ class Node {
         K key;
         V value;
         int level;
-        vector<Node*> next;
+        vector<Node<K, V>*> next;
     public:
+        Node() {};
+        Node(K k, V v, int l) : key(k), value(v), level(l) {}
+        ~Node() {};
+        K getKey() const { return key; }
+        V getValue() const { return value; }
+        int getLevel() const { return level; }
+        void setValue(V v) { value = v;}
+        void setLevel(int l);
+};
+
+template<typename K, typename V>
+class SkipList {
+    private:
+        int max_level;
+        int list_level;
+        int length;
+        Node<K, V>* head;
+        ofstream file_writer;
+        ifstream file_reader;
+    public:
+        SkipList(int ml): max_level(ml), level(0);
+        ~SkipList();
+        int randomLevel();
+        Node<K, V>* createNode(K k, V v, int l);
+        int size() { return length; }
 };
 
